e8dfa4564f0d734714835e9f3eadcc936d1b4e6d
Shai Avruj <shaiavruj@gmail.com>
2022-07-01 10:35:18 -0300

    Initial commit

---
+++ Assets/Codigo/SnakeControl.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SnakeControl : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float giroSpeed = 180;
+    public int Hueco;
+    public GameObject textoPerdiste;
+    public GameObject botonRepetir;
+    public int Score;
+    public Text Puntuacion;
+    public GameObject CuerpoPrefab;
+    public AudioSource Monedita;
+    public AudioSource Muerte;
+    List<GameObject> Cuerpos = new List<GameObject>();
+    List<Vector3> PositionHistory = new List<Vector3>();
+    public Text Tiempo;
+    void Start()
+    {
+        textoPerdiste.SetActive(false);
+        botonRepetir.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Tiempo.text = ""+ Mathf.Floor(Time.time);
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+
+        float giroDireccion = Input.GetAxis("Horizontal");
+
+        transform.Rotate(Vector3.up * giroDireccion * giroSpeed * Time.deltaTime);
+
+        PositionHistory.Insert(0, transform.position);
+
+        for (int i = 0; i < Cuerpos.Count; i++)
+        {
+            GameObject body = Cuerpos[i];
+            Vector3 point = PositionHistory[Mathf.Clamp(i * Hueco, 0, PositionHistory.Count - 1)];
+
+            Vector3 moveDireccion = point - body.transform.position;
+            body.transform.position += moveDireccion * moveSpeed * Time.deltaTime;
+
+            body.transform.LookAt(point);
+        }
+        Puntuacion.text = Score.ToString();
+    }
+
+    void GrowSnake()
+    {
+        GameObject Cuerpo = Instantiate(CuerpoPrefab);
+        Cuerpos.Add(Cuerpo);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Comida")
+        {
+            Monedita.Play();
+            GrowSnake();
+            Destroy(other.gameObject);
+            Score++;
+        }
+        if(other.gameObject.tag == "Pared")
+        {
+            Muerte.Play();
+            textoPerdiste.SetActive(true);
+            Time.timeScale = 0;
+            botonRepetir.SetActive(true);
+        }
+    }
+    public void Repetir()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("SampleScene");
+    }
+}
---
+++ ControlSnake.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlSnake : MonoBehaviour
+{
+    public float MoveSpeed = 5;
+    public float SteerSpeed = 180;
+    public GameObject MyPrefab;
+    public float Insert;
+    private List<GameObject> namesOfDestroyedObjects = new List<GameObject>();
+    private List<Vector3> positionSnake = new List<Vector3>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MySnakePrefab();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+        float steerDirection = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up * steerDirection * SteerSpeed * Time.deltaTime);
+        positionSnake.Insert(0, transform.position);
+        foreach (GameObject prefab in namesOfDestroyedObjects)
+        {
+            int i = 0;
+            Vector3 position = positionSnake[Mathf.Min(i)]
+            i++;
+        }
+
+    }
+    private void MySnakePrefab()
+    {
+        GameObject prefab = Instantiate(MyPrefab);
+        namesOfDestroyedObjects.Add(prefab);
+    }
+   
+}
